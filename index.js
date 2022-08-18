3765487892e67ee3794f899380d7fddceb8dde28
Sylvia Akoth Omollo <sylvia_omollo@gwu.edu>
2022-08-18 22:41:00 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(n){
   // Write your code here!
+let greeting
+  if( n  <= 400 ) {
+    return "This one is on me!";
+  }  if (n >= 2500) {
+    return 'No can do.';
+  } else if(n > 2000) {
+    return "I will gladly take your thirty bucks.";
+}
+}
+function ternaryCheckCity(city){
+  // Write your code here!
+let cityName = city === "NYC" ? 'Ok, sounds good.' :'No go.';
+  return cityName
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+    case 'not as generous':
+      return 'Thank you.';
+      break; 
+    default :
+      return 'Bye.'
+  }
 }
